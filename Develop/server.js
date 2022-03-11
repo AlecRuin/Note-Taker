d8383301771f69171a07c15a71899d17bf7700fe
Alec Harvey <ruinhard@gmail.com>
2022-03-11 14:25:17 -0600

    Added descriptive comments

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+//DECLARE VARIALBES AND ASSETS TO BE USED
 const express = require('express');
 const path = require('path');
 const NoteData = require("./db/db.json")
@@ -5,20 +6,24 @@ const PORT = 3001;
 const uuid = require('./helpers/uuid');
 const fs = require("fs");
 const app = express();
+//USE EXPRESS AND SPECIFY WHICH FOLDER TO BASE LAUNCH OFF OF
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
+//DECLARE INDEX AS MAIN HTML FILE TO USE
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
-
+//API TO SEND BACK THE JSON NOTES
 app.get('/api/notes', (req, res) =>{
     res.json(NoteData)
 });
+//API THAT ROUTES THE NOTE.HTML UPON REQUESTING ACCESS
 app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
+//THE CODE BELOW IS USED FOR DATA PERSISTANCE. IT IS NOT REQUIRED FOR ASSIGNMENT, SO ITS HERE AS PROOF OF KNOWLEDGE
+
 /*app.post('/api/notes', (req, res) => {//:params. 
     // Log our request to the terminal
     if (req.body) {
@@ -47,12 +52,14 @@ app.get('/notes', (req, res) =>{
         }     
     }
 });*/
+
+//WITH THE NOTES BODY RECIEVE, IT WILL CREATE A UUID AND ADD IT TO THE END OF THE NOTEDATA JSON TABLE.
 app.post('/api/notes', (req, res) => { 
     req.body.id = uuid();
     NoteData.push(req.body);
     res.json(NoteData);
 });
-
+//WITH THE ID AS A PARAMETER, IT WILL SEARCH THE NOTEDATA JSON TABLE AND REMOVE THE ENTRY. IF NOTHING COULD BE FOUND, IT WILL THROW ERROR
 app.delete('/api/notes/:id', (req, res) => {
     if (req.body && req.params.id) {
     console.info(`${req.method} request received to delete a note`);
@@ -67,7 +74,7 @@ app.delete('/api/notes/:id', (req, res) => {
     }
     console.log(NoteData)
     if (flag){
-        console.log("SUCCESSFULLY DELETE NOTE!")
+        console.log("SUCCESSFULLY DELETED NOTE!")
         res.json("SUCCESS")
     }else{
         res.json("ERROR. COULDNT FIND NOTE WITH CORRECT ID")
