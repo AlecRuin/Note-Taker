62deccba60853952ec99a2baa813e46f1e86230b
Alec Harvey <ruinhard@gmail.com>
2022-03-11 15:00:19 -0600

    Heroku, but it refuses to work

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const NoteData = require("./db/db.json")
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const uuid = require('./helpers/uuid');
 const fs = require("fs");
 const app = express();
